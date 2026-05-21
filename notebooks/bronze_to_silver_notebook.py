de01520f2a62ea2fd022bcea4566ff6ea16f56f3
HeeJung Kim <hjkim20022@gmail.com>
2026-05-21 02:09:42 -0400

    Add Databricks notebook example

---
+++ notebooks/bronze_to_silver_notebook.py
@@ -0,0 +1,9 @@
+# Databricks Bronze to Silver Example
+
+print("Reading Bronze Layer Data")
+
+print("Cleaning and Transforming Data")
+
+print("Writing Silver Layer Data")
+
+print("Notebook Pipeline Completed")
